2a02ad82fa641bca59b6de337031ac5bd21e3ba8
Barla Ankit <barla.x88@gmail.com>
2023-06-06 22:54:10 +0530

    Implement barcode reading functionality

--- src/Components/BarcodeReader/BarcodeReader.jsx
+++ src/Components/BarcodeReader/BarcodeReader.jsx
@@ -1,21 +1,47 @@
-import { useEffect, useRef, useState } from "react";
-import img from "./qr.png";
+import { useRef } from "react";
 
 const BarcodeReader = () => {
-  const barcodeParser = async () => {
-    const barcodeDetector = new BarcodeDetector({
-      formats: ["qr_code"],
-    });
+  const videoRef = useRef(null);
 
-    const result = await barcodeDetector.detect(document.querySelector("#img"));
-    result.length > 0 ? alert(result[0].rawValue) : alert("no data");
+  const barcodeParser = async () => {
+    try {
+      //media source
+      const src = await navigator.mediaDevices.getUserMedia({ video: true });
+
+      if (src) videoRef.current.srcObject = src;
+
+      if (!window.BarcodeDetector) {
+        throw new Error("Device dosen't have Barcode reading capabilities");
+      }
+
+      const barcodeDetector = new BarcodeDetector({
+        formats: ["qr_code"],
+      });
+
+      const showResult = setInterval(async () => {
+        const result = await barcodeDetector.detect(videoRef.current);
+        if (result.length > 0) alert(result[0].rawValue);
+      }, 1000);
+    } catch (error) {
+      console.log(error.message);
+    }
   };
 
   return (
     <div className="barcode-reader">
-      <button onClick={barcodeParser}>Capture</button>
-
-      {<img src={img} alt="Captured photo" id="img" />}
+      <video ref={videoRef} autoPlay={true}></video>
+      <div>
+        <button onClick={barcodeParser}> Start Capturing Data</button>
+        <button
+          onClick={() => {
+            videoRef.current.srcObject.getTracks().forEach((track) => {
+              track.stop();
+            });
+          }}
+        >
+          Stop Capturing
+        </button>
+      </div>
     </div>
   );
 };
