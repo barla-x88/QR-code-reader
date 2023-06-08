7c076991ce12388cd9f2899679264b7ef381c1b6
Barla Ankit <barla.x88@gmail.com>
2023-06-08 22:27:15 +0530

    Add QR scan result display feature

--- src/Components/BarcodeReader/BarcodeReader.jsx
+++ src/Components/BarcodeReader/BarcodeReader.jsx
@@ -1,10 +1,18 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
+import { ImSearch, ImStop } from "react-icons/im";
+import { FaCopy } from "react-icons/fa";
 
 const BarcodeReader = () => {
   const videoRef = useRef(null);
+  const resultRef = useRef(null);
+  const [isStart, setIsStart] = useState(false);
+  const [inputValue, setInputValue] = useState("https://readqr.netlify.app");
 
-  const barcodeParser = async () => {
+  const readQR = async () => {
     try {
+      // indicate QR reading process is in progress
+      setIsStart(true);
+
       //media source
       const src = await navigator.mediaDevices.getUserMedia({ video: true });
 
@@ -20,28 +28,58 @@ const BarcodeReader = () => {
 
       const showResult = setInterval(async () => {
         const result = await barcodeDetector.detect(videoRef.current);
-        if (result.length > 0) alert(result[0].rawValue);
+        if (result.length > 0) {
+          setInputValue(result[0].rawValue);
+          stopReadQR();
+        }
       }, 1000);
     } catch (error) {
       console.log(error.message);
     }
   };
 
+  const stopReadQR = () => {
+    videoRef.current.srcObject.getTracks().forEach((track) => {
+      track.stop();
+
+      //indicate QR reading has stoped
+      setIsStart(false);
+    });
+  };
+
+  const copyToClipboard = () => {
+    resultRef.current.select();
+    navigator.clipboard
+      .writeText(resultRef.current.value)
+      .catch((error) => console.log(error));
+  };
+
   return (
     <div className="barcode-reader">
-      <video ref={videoRef} autoPlay={true}></video>
-      <div>
-        <button onClick={barcodeParser}> Start Capturing Data</button>
+      <div className="video-container">
+        <video ref={videoRef} autoPlay={true}></video>
         <button
-          onClick={() => {
-            videoRef.current.srcObject.getTracks().forEach((track) => {
-              track.stop();
-            });
-          }}
+          type="button"
+          className="start-btn"
+          onClick={!isStart ? readQR : stopReadQR}
         >
-          Stop Capturing
+          {!isStart ? <ImSearch style={{ color: "green" }} /> : <ImStop />}
         </button>
       </div>
+
+      <input
+        className="result-text"
+        type="text"
+        value={inputValue}
+        readOnly={true}
+        placeholder="Result will appear here.."
+        style={{ textAlign: "center" }}
+        ref={resultRef}
+      />
+
+      <button type="button" className="copy-btn" onClick={copyToClipboard}>
+        <FaCopy />
+      </button>
     </div>
   );
 };
